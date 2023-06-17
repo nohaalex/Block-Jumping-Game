65f682502b9b9f7f6909cd4a67bad143f0dca025
nohaalex <alexnoha37@gmail.com>
2023-06-17 12:38:29 +0530

    Add project files.

---
+++ Blockgame/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump() {
+	if (character.classList != "animate") {
+		character.classList.add("animate");
+	}
+	setTimeout(function () {
+		character.classList.remove("animate");
+	},500)
+}
+
+var checkDead = setInterval(function() {
+	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+	var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+	if (blockleft < 40 && blockleft > 20 && characterTop >= 130) {
+		block.style.animation = "none";
+		block.style.display = "none";
+		alert("You Lose");
+
+	}
+
+}, 10);
